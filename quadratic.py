0c4554ab4d084710350b26ec29dd1fe8d609dfef
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-03-04 08:48:38 +0200

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,31 +1,44 @@
-print("--Welcome to Quadratic Equation Solver--")
-print("[1] Start")
-print("[2] Stop")
+import os
+import math
 
-user_choice = int(input("Enter your choice: "))
-while user_choice == 1 and user_choice != 2:
-    try:
-        os.system('cls')
-        a = int(input("Enter a: "))
-        b = int(input("Enter b: "))
-        c = int(input("Enter c: "))
-        d = (b ** 2) - 4 * a * c
-        print(f"The Formula: {a}x**2 + {b}x + {c}")
-        form = (-b + math.sqrt(abs(d))) / (2 * a)
-        form2 = (-b - math.sqrt(abs(d))) / (2 * a)
-        if d < 0:
-            print("Complex Roots")
-            print(-b / (2 * a), "+", f"{math.sqrt(abs(d))}i")
-            print(-b / (2 * a), "-", f"{math.sqrt(abs(d))}i")
-        elif d == 0:
-            print("One real root")
-            print('X:', form)
-        else:
-            print("Two real roots")
-            print('X1:', form)
-            print('X2:', form2)
+print("---- Welcome to Quadratic Equation Solver ----")
+print("[1] Solve Quadratic Equation")
+print("[2] Exit")
+
+while True:
+    user_choice = input("Enter your choice (1/2): ")
+    if user_choice == "1":
+        try:
+            os.system('cls')
+            a = float(input("Enter a: "))
+            if a == 0:
+                print("Error! 'a' can not be zero.")
+                continue
+            b = float(input("Enter b: "))
+            c = float(input("Enter c: "))
+            d = (b ** 2) - 4 * a * c
+            print(f"The Quadratic Equation: {a}x**2 + {b}x + {c}")
+            if d < 0:
+                print("The quadratic equation has complex roots")
+                real_part = -b / (2 * a)
+                imaginary_part = math.sqrt(abs(d)) / (2 * a)
+                print(f"Roots: {real_part} + {imaginary_part}i and {real_part} - {imaginary_part}i")
+            elif d == 0:
+                print("The quadratic equation has one real root")
+                root = -b / (2 * a)
+                print(f"Root: {root}")
+            else:
+                print("The quadratic equation has two real roots")
+                root1 = (-b + math.sqrt(d)) / (2 * a)
+                root2 = (-b - math.sqrt(d)) / (2 * a)
+                print(f"Roots: {root1} and {root2}")
+        except ValueError:
+            os.system('cls')
+            print("Invalid input! Please enter a valid number.")
+        except ZeroDivisionError:
+            os.system('cls')
+            print("Error! 'a' can not be zero.")
+    elif user_choice == "2":
         break
-    except:
-        os.system('cls')
-        print("Enter a valid A! ")
-        
+    else:
+        print("Invalid choice! Please enter a valid choice (1/2).")
